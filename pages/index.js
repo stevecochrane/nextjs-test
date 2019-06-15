83c4a97335fb4b11075b6877683783a36856eac2
Steve Cochrane <steve@stevecochrane.com>
2019-06-14 21:19:42 -0500

    Add custom page title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+	return (
+		<div>
+			<Head>
+				<title>Custom Page Title</title>
+			</Head>
+			<div>Welcome to Next.js!</div>
+		</div>
+	);
 }
 
 export default Home;
