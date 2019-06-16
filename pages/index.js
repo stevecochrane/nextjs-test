fdef970ab52201d3cc45086d76a860ef56638bf3
Steve Cochrane <steve@stevecochrane.com>
2019-06-15 20:15:15 -0500

    Add some quick test styles with Emotion to index page header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import { css } from '@emotion/core';
+
+const headerStyles = css`
+	background-color: #ccc;
+	color: #444;
+	font-family: sans-serif;
+	font-size: 2rem;
+`;
 
 function Home() {
 	return (
@@ -7,7 +15,7 @@ function Home() {
 			<Head>
 				<title>Custom Page Title</title>
 			</Head>
-			<div>Welcome to Next.js!</div>
+			<h1 css={headerStyles}>Welcome to Next.js!</h1>
 		</React.Fragment>
 	);
 }
