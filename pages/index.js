925256de50362797dcdbf79e9adc3e2e202e9463
Steve Cochrane <steve@stevecochrane.com>
2019-06-16 19:16:21 -0500

    Add an initial asynchronous fetch to the Home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-fetch';
 import Head from 'next/head';
 import Link from 'next/link';
 import { css } from '@emotion/core';
@@ -10,18 +12,38 @@ const headerStyles = css`
 	font-size: 2rem;
 `;
 
-function Home() {
-	return (
-		<React.Fragment>
-			<Head>
-				<title>Custom Page Title</title>
-			</Head>
-			<h1 css={headerStyles}>Welcome to Next.js!</h1>
-			<Link href="/about">
-				<a>About Page</a>
-			</Link>
-		</React.Fragment>
-	);
+class Home extends React.Component {
+	static async getInitialProps() {
+		const url = 'https://api.github.com/users/gaearon/gists';
+		const response = await fetch(url);
+		const gists = await response.json();
+		return { gists };
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<Head>
+					<title>Custom Page Title</title>
+				</Head>
+				<h1 css={headerStyles}>Welcome to Next.js!</h1>
+				<Link href="/about">
+					<a>About Page</a>
+				</Link>
+				<h2>Gists by GitHub user gaearon:</h2>
+				{this.props.gists &&
+					<ul>
+						{this.props.gists.map(gist => (
+							<li key={gist.id}>{gist.description}</li>
+						))}
+					</ul>
+				}
+			</React.Fragment>
+		);
+	}
 }
+
+Home.propTypes = {
+	gists: PropTypes.array
+};
 
 export default Home;
